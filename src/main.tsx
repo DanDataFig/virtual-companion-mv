509b2556e54f80476ae9a575a135089808315588
Dan <31245523+DanDataFig@users.noreply.github.com>
2025-08-01 11:59:04 +0000

    Generated by Spark: Fix all reported errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "@github/spark/spark"
-import "@github/spark/spark"
-
-
+import { ErrorBoundary } from 'react-error-boundary'
+import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
-
+import "@github/spark/spark"
 import "./main.css"
-    <App />
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
-   </ErrorBoundary>
-
+  <StrictMode>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>,
+)
